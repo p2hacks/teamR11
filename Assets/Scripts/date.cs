5a8db5e449bc35af5cec221f66b421867af4d0a0
Yuto Mori <b1016206@fun.ac.jp>
2019-12-12 15:56:48 +0900

    add date text

---
+++ Assets/Scripts/date.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TMPro;
+
+public class date : MonoBehaviour
+{
+    
+    //[SerializeField]
+    public TextMeshPro textbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dt = DateTime.Now;
+
+        Debug.Log(dt.Year);         // 現在日付の年部分を取得
+        Debug.Log(dt.Month);        // 現在日付の月部分を取得
+        Debug.Log(dt.Day);          // 現在日付の日部分を取得
+        Debug.Log(dt.DayOfWeek);  // 現在日付の曜日部分を取得
+        textbox = this.GetComponent<TextMeshPro>();
+        textbox.text = dt.Year +"/"+ dt.Month + "/"+ dt.Day;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
